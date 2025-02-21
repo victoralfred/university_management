c63affd6865d4c68728d64c30758f3271538843c
Victor Oseghale <voseghale1@gmail.cm>
2025-02-21 05:19:02 +0100

    Added un versioned files

--- security-service/src/test/java/com/vickezi/security/dao/reads/GroupAndRoleServiceImplTest.java
+++ security-service/src/test/java/com/vickezi/security/dao/reads/GroupAndRoleServiceImplTest.java
@@ -1,29 +1,14 @@
 package com.vickezi.security.dao.reads;
 
-import com.vickezi.security.dao.UserAuthoritiesDTO;
-import com.vickezi.security.model.Users;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.r2dbc.core.DatabaseClient;
-import org.springframework.r2dbc.core.RowsFetchSpec;
-import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
-import reactor.test.StepVerifier;
-import java.util.UUID;
-import java.util.function.Function;
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-
 
 @SpringBootTest
 class GroupAndRoleServiceImplTest {
+    @Test
+    public void runtiest() {
+        System.out.println("None");
+    }
 
 }
