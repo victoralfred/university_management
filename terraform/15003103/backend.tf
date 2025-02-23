e048bea8a2984d9e024ba601f65d520282948766
Victor Oseghale <voseghale1@gmail.cm>
2025-02-23 16:03:25 +0100

    Nonp

--- terraform/15003103/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-bucket-voseghale"
-    key            = "15003103/02ebe298-1037-4fbf-88dd-a3115930aeed/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "TerraformStateLocks"
-  }
-}
