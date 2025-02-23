b643248aff88230a405457a2d1f55e3fad3e3e52
Victor Oseghale <voseghale1@gmail.cm>
2025-02-23 16:10:40 +0100

    Added the process runner

---
+++ processor/src/main/java/com/vickezi/processor/ProcessService.java
@@ -0,0 +1,11 @@
+package com.vickezi.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcessService {
+    public static void main(String[] args) {
+        SpringApplication.run(ProcessService.class, args);
+    }
+}
